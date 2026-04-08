58a0be662d2c3f535a69397fe6e4e9750c9233f2
Rakshit Badiger <rakshitbadiger2004@gmail.com>
2026-04-09 00:48:55 +0530

    All origin

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://localhost:5173"
-                  ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

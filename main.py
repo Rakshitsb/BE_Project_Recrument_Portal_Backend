b34bfc0013f6e61aabfa9f0ba99c886478a89546
Rakshit Badiger <167094343+Rakshitsb@users.noreply.github.com>
2026-04-08 23:11:47 +0530

    Add HTTPS origin to CORS middleware

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://localhost:5173"
+                  ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
